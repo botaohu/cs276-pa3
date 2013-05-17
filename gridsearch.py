b2977925d4c5af9c5e4e317406846634aeeb5685
Botao Hu <botaohu@corn05.stanford.edu>
2013-05-17 13:56:12 -0700

    grid

--- gridsearch.py
+++ gridsearch.py
@@ -1,4 +1,5 @@
 import scipy
+import scipy.optimize
 import rank
 import ndcg
 import numpy as np
@@ -7,16 +8,17 @@ def tune(w1,w2,w3,w4,w5):
 	rank.main(1,"queryDocTrainData")
 	return -ndcg.main("ranked.txt", "queryDocTrainRel")
 
-[x0,fval,grid,jout] = scipy.optimize.brute(tune, 
-	np.linspace(0.0, 3.0, num=1),
-	np.linspace(0.0, 3.0, num=1),
-	np.linspace(0.0, 3.0, num=1),
-	np.linspace(0.0, 3.0, num=1),
-	np.linspace(0.0, 3.0, num=1))
+#[x0,fval,grid,jout] = scipy.optimize.brute(tune, 
+#	np.linspace(0.0, 3.0, num=1),
+#	np.linspace(0.0, 3.0, num=1),
+#	np.linspace(0.0, 3.0, num=1),
+#	np.linspace(0.0, 3.0, num=1),
+#	np.linspace(0.0, 3.0, num=1))
 
 def tune2(w):
 	rank.weights = w
 	rank.main(1,"queryDocTrainData")
+	print w
 	return -ndcg.main("ranked.txt", "queryDocTrainRel")
 
 print scipy.optimize.anneal(tune2, [1.0, 0.5, 0.1, 0.3, 2.0])
